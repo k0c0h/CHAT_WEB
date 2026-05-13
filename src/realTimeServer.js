6247f5013dbbffcf02c9beab5e47c573648c11a5
Kerlly Alexandra Chiriboga Cabrera <chiribogakerly@gmail.com>
2026-05-12 19:44:20 -0500

    cambios y disenio

--- src/realTimeServer.js
+++ src/realTimeServer.js
@@ -1,7 +1,33 @@
-module.exports = httpServer => {
-    const {Server } = require("socket.io");
-    const io = new Server(httpServer);
-    io.on("connection", socket => {
-        console.log("Te conectaste al socket ", socket.id)
-    })
+module.exports = (httpServer) => {
+  const { Server } = require("socket.io");
+  const io = new Server(httpServer);
+
+  const getUserFromCookie = (cookieHeader = "") => {
+    const cookie = cookieHeader
+      .split(";")
+      .map((item) => item.trim())
+      .find((item) => item.startsWith("username="));
+
+    return cookie ? decodeURIComponent(cookie.split("=").slice(1).join("=")) : "Usuario";
+  };
+
+  io.on("connection", (socket) => {
+    const user = getUserFromCookie(socket.request.headers.cookie);
+
+    socket.on("message", (message) => {
+      io.emit("message", {
+        user,
+        message,
+        date: new Date().toLocaleTimeString(),
+      });
+    });
+
+    socket.on("typing", () => {
+      socket.broadcast.emit("typing", { user });
+    });
+
+    socket.on("stopTyping", () => {
+      socket.broadcast.emit("stopTyping", { user });
+    });
+  });
 };
